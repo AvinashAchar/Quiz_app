70696f54480fc0340b4d27a5e46a79e74b6dfa66
AvinashAchar <avinashacharya121@gmail.com>
2023-07-29 21:01:57 +0530

    This Is My First Commit

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quizapp;
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "The Indian Institute of Science is located at?",
+            "Which is main River og Karnataka?",
+            "Who is the Mother of Koti and Chennaya?",
+            "Where is Shri Parashurama Theme Park located in Karnataka ?"
+    };
+
+    public static String choices[][] = {
+            {"Kerala","Madras","Banglore","New Delhi"},
+            {"Kaveri","Krishna","Godavari","None of these"},
+            {"Deyi Baidithi","Kinnidaru","Chennamma","Ponnamma"},
+            {"Manglore","Udupi","Banglore","Mysore"}
+    };
+
+    public static String correctAnswers[] = {
+            "Banglore",
+            "Kaveri",
+            "Deyi Baidithi",
+            "Udupi"
+    };
+
+}
